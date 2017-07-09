1c2832923580de2de2f348a3a7a8a439a2ea029c
Antonio Terrero <atalgaba@gmail.com>
2017-07-09 18:25:23 +0200

    change log loc

--- lib/Log.php
+++ lib/Log.php
@@ -22,7 +22,7 @@ class Log {
 
 	public static function out($str) {
 		$str = explode("\n", $str);
-		$f = fopen('/var/log/timings.log', 'a');
+		$f = fopen('../log/timings.log', 'a');
 		foreach ($str as $line) {
 			$log = $_SERVER['REMOTE_ADDR'] . ":" . getmypid() . "\t[" . date(DATE_W3C) . "]\t$line\n";
 			fwrite($f, $log);
