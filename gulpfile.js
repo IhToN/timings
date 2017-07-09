3dd74418c03d8126fc0462010af0334893d00f47
Antonio Terrero <atalgaba@gmail.com>
2017-07-09 17:16:46 +0200

    ahuuu

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,3 @@ gulp.task('default', () => {
   let config = webpackConfig(process.env.NODE_ENV === "production", true);
   webpack(config, config.reporter());
 });
-
-gulp.task('heroku:production', function(){
-  runSeq('build:prod'')
-})
